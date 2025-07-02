825d35f36adff9218603bd982c223af2c357cbaa
SohamXYZ <144525667+SohamXYZDev@users.noreply.github.com>
2025-07-02 16:11:24 +0530

    Add files via upload

--- commands/sendSupportEmbed.js
+++ commands/sendSupportEmbed.js
@@ -11,6 +11,7 @@ module.exports = {
       .setTitle("KingsBet - Support")
       .setDescription("Click on the button to create a support ticket")
       .setColor(0x41fb2e)
+      .setImage("https://media.discordapp.net/attachments/1374310263003807778/1384544261814489109/Untitled_design_-_2025-06-17T162721.029_1.png?ex=6856c553&is=685573d3&hm=b6976c7cb0dc0a9ee6ceed3c1f3aa022e3b896f8e404c1f793b67ceda4de15f8&=&format=webp&quality=lossless")
 
     // Create the button
     const button = new ButtonBuilder()
